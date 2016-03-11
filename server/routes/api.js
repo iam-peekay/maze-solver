b60b37ce804aa095a092301c64955f8ca3ad18f0
Preethi <iam.preethi.k@gmail.com>
2016-03-11 12:02:26 -0800

    Update how maze traverses board

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,10 +8,10 @@ var currentPath;
 const checkValidAsync = (id, i, j) => {
   axios.get(`${baseURL}/maze/${id}/check?x=${i}&y=${j}`)
     .then(function(response) {
-      currentBoard[i][j] = true;
+      currentBoard[i][j] = 1;
     })
     .catch(function (error) {
-      currentBoard[i][j] = false;
+      currentBoard[i][j] = 0;
     });
 }
 
@@ -21,6 +21,7 @@ const makeBoard = (id, height, width, board) => {
       board.push([]);
       funcs.push([]);
       for (var j = 0; j < width; j++) {
+        board[i][j] = 'todo';
         funcs[i][j] = checkValidAsync.bind(null, id, i, j)();
       }
     }
@@ -52,14 +53,14 @@ const solvePuzzle = (currentMaze, currentBoard) => {
 
   // Inner helper function to find path
   function findPath(id, board, col, row, path, memo) {
-    if (row < 0 || col < 0 || !board[col][row]) {
+    if (row < 0 || col < 0 || row > lastRow || col > lastColumn || (board[row][col] === 0)) {
       return false;
     }
 
     // Strinify the current point so we can use it as the key
     // for our hash table
-    var point = `{x: ${col}, y: ${row}}`;
-    if (memo[point]) {
+    var point = `{x: ${row}, y: ${col}}`;
+    if (memo[point] !== undefined) {
       return memo[point];
     }
 
@@ -70,20 +71,18 @@ const solvePuzzle = (currentMaze, currentBoard) => {
     // I make the assumption that we can only move right, up or
     // diagonal up (right + up)
     var pathExists = false;
-
+    // findPath(id, board, col - 1, row - 1, path, memo)
     // If we are at the origin, we know that the path exists
     var atOrigin = row === 0 && col === 0;
     // Check all adjacent points, working backwards
-    if (atOrigin || findPath(id, board, col, row - 1, path, memo) || findPath(id, board, col - 1, row, path, memo) || findPath(id, board, col - 1, row - 1, path, memo)) {
-      path.push({x: col, y: row});
+    if (atOrigin || findPath(id, board, col, row - 1, path, memo) || findPath(id, board, col - 1, row, path, memo)) {
+      path.push({x: row, y: col});
       pathExists = true;
     }
     memo[point] = pathExists;
     return pathExists;
   }
 }
-
-
 
 module.exports = (app) => {
   // Get current maze via Krypton API and build up board based
@@ -104,21 +103,23 @@ module.exports = (app) => {
   // Solve current maze and send path solution as response
   app.get('/solvemaze', (req, res) => {
     currentPath = solvePuzzle(currentMaze, currentBoard);
-    res.status(200).send({'Path to end of maze': currentPath});
+    res.status(200).send({'Path to end of maze': currentPath, 'Current board': currentBoard});
   });
 
   // Submit maze to the Krypton API
   app.get('/submitmaze', (req, res) => {
     var id = currentMaze.id;
+    console.log(currentMaze, currentPath);
+    console.log(currentBoard);
     axios.post(`${baseURL}/maze/${id}/solve`, currentPath)
       .then((response) => {
+        console.log(response);
         if (response.status === 200) {
           res.status(200).send(response);
-        } else {
-          res.status(500).end();
         }
       })
       .catch((error) => {
+        res.status(500).send(error);
         throw new Error(error.stack);
       });
   });
